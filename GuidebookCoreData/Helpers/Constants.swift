58dcb3327d2d2276beff6206fbf3fc57d15f0f5e
Brian Dilts <brianedilts@gmail.com>
2021-05-26 11:19:33 -0600

    - add view controllers for map, notes, info, and places - add constants file and make constants for sub VCs - add storyboard elements: style first screen and ad VCs for sub views

---
+++ GuidebookCoreData/Helpers/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  GuidebookCoreData
+//
+//  Created by Brian Dilts on 5/26/21.
+//
+
+import Foundation
+
+class Constants {
+    
+    static var INFO_VIEWCONTROLLER = "InfoVC"
+    static var MAP_VIEWCONTROLLER = "MapVC"
+    static var NOTES_VIEWCONTROLLER = "NotesVC"
+}
